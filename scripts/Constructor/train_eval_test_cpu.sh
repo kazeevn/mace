ca39c8fd17e76598ee5be8ae67714471063a2ba1
Nikita Kazeev <nikita.kazeev@sit.org>
2023-10-09 12:09:44 +0000

    CRP scripts

--- scripts/Constructor/train_eval_test_cpu.sh
+++ scripts/Constructor/train_eval_test_cpu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+source ./get_data.sh
 cd $HOME/project
 
 # TODO(kazeevn) limit pytorch's CPU ambitions to ROLOS_AVAILABLE_CPU
-
 python3 ./scripts/run_train_.py \
   --name="MACE_model" \
-  --train_file="BOTNet-datasets/dataset_3BPA/train_300K.xyz" \
+  --train_file="$DATA_FOLDER/dataset_3BPA/train_300K.xyz" \
   --valid_fraction=0.05 \
-  --test_file="BOTNet-datasets/dataset_3BPA/test_300K.xyz" \
+  --test_file="$DATA_FOLDER/dataset_3BPA/test_300K.xyz" \
   --E0s='{1:-13.663181292231226, 6:-1029.2809654211628, 7:-1484.1187695035828, 8:-2042.0330099956639}' \
   --model="ScaleShiftMACE" \
   --hidden_irreps='32x0e' \
--- scripts/Constructor/train_eval_test_gpu_400_epochs.sh
+++ scripts/Constructor/train_eval_test_gpu_400_epochs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd $HOME/project
+source ./get_data.sh
 
+cd $HOME/project
 python3 ./scripts/run_train_.py \
   --name="MACE_model" \
-  --train_file="BOTNet-datasets/dataset_3BPA/train_300K.xyz" \
+  --train_file="$DATA_FOLDER/dataset_3BPA/train_300K.xyz" \
   --valid_fraction=0.05 \
-  --test_file="BOTNet-datasets/dataset_3BPA/test_300K.xyz" \
+  --test_file="$DATA_FOLDER/dataset_3BPA/test_300K.xyz" \
   --E0s='{1:-13.663181292231226, 6:-1029.2809654211628, 7:-1484.1187695035828, 8:-2042.0330099956639}' \
   --model="ScaleShiftMACE" \
   --hidden_irreps='32x0e' \
