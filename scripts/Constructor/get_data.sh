c0dbacfb0a4f7fd5e8b656dda4fac27b41343496
Nikita Kazeev <nikita.kazeev@sit.org>
2023-10-09 12:14:17 +0000

    bash export

--- scripts/Constructor/get_data.sh
+++ scripts/Constructor/get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATA_FOLDER="/tmp/BOTNet-datasets"
+export DATA_FOLDER="/tmp/BOTNet-datasets"
 git clone https://github.com/davkovacs/BOTNet-datasets.git $DATA_FOLDER
 cd $DATA_FOLDER
 git reset --hard 29e6d467317e4b5967b7ea5cbee54de953fa0d45
