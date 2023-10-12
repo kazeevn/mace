365d8f4db4c9afc42e99e2c2977a45f1e5fb3f83
Igor Makarov <igor.makarov@constructor.tech>
2023-10-12 10:31:06 +0000

    data added manually

--- scripts/Constructor/train_eval_test_cpu.sh
+++ scripts/Constructor/train_eval_test_cpu.sh
@@ -3,6 +3,7 @@
 python3 scripts/run_train_.py \
   --name="MACE_model" \
   --train_file="BOTNet-datasets/dataset_3BPA/train_300K.xyz" \
+  --checkpoints_dir="/output"\
   --valid_fraction=0.05 \
   --test_file="BOTNet-datasets/dataset_3BPA/test_300K.xyz" \
   --E0s='{1:-13.663181292231226, 6:-1029.2809654211628, 7:-1484.1187695035828, 8:-2042.0330099956639}' \
